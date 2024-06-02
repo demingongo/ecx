4dfe858c0c34853375d9edbadb3194b0daeee47d
demingongo <sdemingongo@gmail.com>
2024-06-02 15:07:52 +0200

    Update apply.go

--- cmd/apply.go
+++ cmd/apply.go
@@ -13,13 +13,48 @@ import (
 // applyCmd represents the apply command
 var applyCmd = &cobra.Command{
 	Use:   "apply",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Apply ecx.yaml project file",
+	Long: `Apply ecx.yaml project file.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+The ecx.yaml project file should locate the resources to deploy.
+For example:
+
+ecx.yaml
++---------------------------------------------------+
+| api: ecx                                          |
+| apiVersion: 0.1                                   |
+|                                                   |
+| ## cloudwatch log groups                          |
+| logGroups:                                        |
+|   - group: /etc/app-test                          |
+|     retention: 1                                  |
+|   - group: /etc/app2                              |
+|                                                   |
+| # ecs task definitions                            |
+| taskDefinitions:                                  |
+|   - taskdefinitions/taskdefinition.json           |
+|   - taskdefinitions/taskdefinition2.json          |
+|                                                   |
+| # flow:                                           |
+| #                                                 |
+| # A flow could be                                 |
+| # - rules, target group and service               |
+| # - rules and target group                        |
+| # - target group and service                      |
+| # - target group                                  |
+| # - or service                                    |
+| #                                                 |
+| # If you specify a target group for a service,    |
+| # that service should have a container port       |
+| # mapping named "http" in its task definition.    |
+| flows:                                            |
+|   - name: app-test-flow                           |
+|     service: services/service.json                |
+|     targetGroup: targetgroups/targetgroup.json    |
+|     rules:                                        |
+|       - rules/rule.json                           |
++---------------------------------------------------+
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		globals.LoadGlobals()
 		applyapp.Run()
@@ -38,7 +73,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// applyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	applyCmd.PersistentFlags().StringP("project", "p", "", "cluster name")
+	applyCmd.PersistentFlags().StringP("project", "p", "", "path to the folder with ecx.yaml")
 	applyCmd.MarkPersistentFlagDirname("project")
 
 	viper.BindPFlag("project", applyCmd.PersistentFlags().Lookup("project"))
